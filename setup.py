60752cc26b9017619e800f1f00a41d06b1fb6671
rjutur <rjutur@umich.edu>
2023-12-31 04:06:18 -0500

    finished app

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ from setuptools import setup
 APP = ['gui.py']
 OPTIONS = {
     'argv_emulation': False,
+    'packages': ['certifi'],
     'plist': {
-        'CFBundleShortVersionString': '1.0',
-        'LSUIElement': True,
-        'com.apple.security.filesystem.full-disk-access': True,
-        # Add any other entitlements you need
+        'CFBundleIdentifier': 'com.ritij.imessagewrapped',  # Replace with your app's bundle ID
+        'CFBundleName': 'iMessageWrapped',  # Replace with your app's name
+        'CFBundleShortVersionString': '1.0',  # Replace with your app's version
+        'CSResourcesFileMapped': True,
+        'NSAppleEventsUsageDescription': 'Explanation of AppleEvents usage',
+        'NSCameraUsageDescription': 'Explanation for camera usage',  # Add other required descriptions
+        'NSMicrophoneUsageDescription': 'Explanation for microphone usage',  # Add other required descriptions
+        'NSRemindersUsageDescription': 'Explanation for reminders usage',  # Add other required descriptions
+        'NSCalendarsUsageDescription': 'Explanation for calendars usage',  # Add other required descriptions
+        'Entitlements': './MyApp.entitlements',  # Path to your entitlements file
     }
 }
 
